adc36abe3bf0dbf99b2e047c8b4c6eb17463bfa2
Yoni Calsin <yonicb20@gmail.com>
2020-03-09 15:54:19 -0500

    data: 🗂️ Created parseTyped function for aggregated type !

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
-import { More } from './util';
-
+import { More, ToBoolean, ToNumber, ToObject } from './util';
 export type Mode = 'auto' | 'customized';
 
 interface TypeCogenvOptions {
@@ -10,10 +9,60 @@ const defaultOptions: TypeCogenvOptions = {
    mode: 'auto',
 };
 
-export const TypeCogenv = (data: More, options: TypeCogenvOptions = {}) => {
+export const parseTyped = (
+   key: string,
+   source: string,
+   mode: Mode = defaultOptions.mode,
+): [string, any] => {
+   const arrKey = key.split('@');
+   const type = arrKey[1];
+   const k = arrKey[0];
+   let value: any = source;
+   if (type) {
+      switch (type) {
+         case 'string':
+            value = '' + source;
+            break;
+         case 'boolean':
+            value = ToBoolean(source);
+            break;
+         case 'number':
+            value = ToNumber(source);
+            break;
+         case 'object':
+         case 'array':
+            value = ToObject(source);
+            break;
+      }
+   } else if (mode == 'auto') {
+      value = ToBoolean(source);
+      value = ToNumber(value);
+      value = ToObject(value);
+   }
+
+   return [k, value];
+};
+
+export const CogenvType = (data: More, options: TypeCogenvOptions = {}) => {
    options = {
       ...defaultOptions,
       ...options,
    };
-   return data;
+
+   let payload: More = {};
+
+   if (data.parsed) {
+      data = data.parsed;
+   }
+
+   for (const [k, v] of Object.entries(data)) {
+      let [key, value] = parseTyped(k, v, options.mode);
+      payload[key] = value;
+   }
+
+   return payload;
 };
+
+export default CogenvType;
+
+module.exports = CogenvType;
