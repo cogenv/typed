1477222e71183b05ccbe00c89146bb76130eb33c
Yoni Calsin <yonicb20@gmail.com>
2020-03-22 18:52:12 -0500

    fix: 🐛 Removed object functions !

--- src/util.ts
+++ src/util.ts
@@ -1,15 +1,6 @@
 export interface More {
    [key: string]: any;
 }
-export const Log = (msg: string) => {
-   console.warn('[CogEnv][Typed][Warning...] ' + msg);
-};
-
-export const StringToObject = (source: string): More => {
-   var data = source.replace(/(['"])?([a-z0-9A-Z_]+)(['"])?:/g, '"$2": ');
-   data = data.replace(/\'/gi, '"');
-   return JSON.parse(data);
-};
 
 export const ToBoolean = (value: any): Boolean => {
    if (typeof value !== 'string') {
@@ -17,12 +8,10 @@ export const ToBoolean = (value: any): Boolean => {
    }
    switch (value.toLowerCase().trim()) {
       case 'true':
-      case '0':
       case 'yes':
          value = true;
          break;
       case 'false':
-      case '0':
       case 'no':
          value = false;
          break;
@@ -36,14 +25,4 @@ export const ToNumber = (value: any): Number => {
       value = parseFloat(value);
    }
    return value;
-};
-
-export const ToObject = (source: any): More => {
-   try {
-      let data = StringToObject(source);
-      if (data) {
-         source = data;
-      }
-   } catch {}
-   return source;
 };
